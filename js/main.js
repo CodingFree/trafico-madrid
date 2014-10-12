9ea066a2ef77feb6d1e68175b0e4597c8f904e4e
Your Name <you@example.com>
2014-10-12 17:52:49 +0200

    Cambiada la orientación principal

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /* jshint esnext: true, browser: true, easy: true */
 (function(window) {
     'use strict';
+    screen.mozLockOrientation("landscape");
     // Prefetch it to cache, then change location.
     var iframe = document.createElement('iframe');
     iframe.setAttribute('src', 'http://informo.munimadrid.es/informo/tmadrid/tmadrid.php');
